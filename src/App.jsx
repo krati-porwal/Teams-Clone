76c77457734723a1dec279ba8cf5c0cee0c2e8ec
krati-porwal <porwalkrati095@gmail.com>
2025-06-01 23:31:00 +0530

    Modification of SCSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './styles/App.scss';
 import Navbar from './Components/HeroSection/navbar';
-import  MainContent from './Components/HeroSection/MainContentFixed';
+import MainContent from './Components/HeroSection/MainContentFixed';
 import SmallBusiness from './Components/Small_business/SmallBusiness';
 import Streamline from './Components/Streamline/Streamline';
 import Price from './Components/Prices/Price';
 import Explore from './Components/Explore_apps/Explore';
 import Reach from './Components/Expand_your_reach/reach';
 import AddToTeam from './Components/Add_to_teams/AddToTeams';
-import Help  from './Components/Help/Help';
+import Help from './Components/Help/Help';
 import MicrosoftPlan from './Components/MicrosoftPlan/MicrosoftPlan';
 
 
@@ -15,16 +14,16 @@ function App() {
 
   return (
     <>
-     <Navbar/>
-     <MainContent/>
-     <SmallBusiness/>
-     <Streamline/>
-     <Price/>
-     <Explore/>
-     <Reach/>
-     <AddToTeam/>
-     <Help/>
-     <MicrosoftPlan/>
+      <Navbar />
+      <MainContent />
+      <SmallBusiness />
+      <Streamline />
+      <Price />
+      <Explore />
+      <Reach />
+      <AddToTeam />
+      <Help />
+      <MicrosoftPlan />
     </>
   )
 }
